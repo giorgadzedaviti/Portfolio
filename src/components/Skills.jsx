89d9df3b90dc9406be33c4d85a5ce1989f50cf9d
dati giorgadze <83769327+niko940@users.noreply.github.com>
2023-01-30 16:18:40 +0400

    UPDATE: minor changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,9 +12,9 @@ const skills = () => {
 <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-red-600 '>Skills</p>
-              <p className='py-4'>These are the technologies I've worked with</p>
+          <div className='mt-5'>
+              <p className=' text-3xl font-bold inline border-b-4 border-red-600 '>Skills</p>
+              <p className=' py-4'>These are the technologies I've worked with</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
